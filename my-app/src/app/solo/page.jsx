e3665485a5ec618d8cdaf7fede16860aca958afa
JahadB <beylercahad@gmail.com>
2025-05-13 16:42:41 +0300

    Added waiting lobby

--- my-app/src/app/solo/page.jsx
+++ my-app/src/app/solo/page.jsx
@@ -81,15 +81,18 @@ export default function SinglePLayer() {
 
     return(
     <div className="flex flex-col justify-center items-center h-screen before:bg-[url('/bg-join.jpg')] before:bg-no-repeat before:bg-cover before:content=[''] before:absolute before:inset-0 before:opacity-30">
-        <div className="isolate bg-fuchsia-900 p-6 rounded-md">
-          <h3 className="font-bold text-2xl">{activeQuestion + 1}. {testQuizData[activeQuestion].question}</h3>
-          <div className="flex flex-col justify-start flex-wrap items-baseline m-2">
+        <div className="isolate bg-[#7E08D8] p-6 rounded-md">
+          <div className="">
+            <h1>Question {activeQuestion + 1}</h1>
+            <h3 className="font-bold text-2xl">{testQuizData[activeQuestion].question}</h3>
+          </div>
+          <div className="flex flex-col justify-start flex-wrap items-baseline m-2 gap-2.5">
             {testQuizData[activeQuestion].options.map((opt, i) => (
-              <button key={i} onClick={() => setSelectedAnswer(i)} style={selectedAnswer === i ? {backgroundColor: "white", color: "black"} : {}} className="text-left p-1.5 rounded-md">{opt}</button>
+              <button key={i} onClick={() => setSelectedAnswer(i)} style={selectedAnswer === i ? {backgroundColor: "white", color: "black"} : {}} className="min-w-[200px] cursor-pointer text-left p-2.5 rounded-md bg-[#F7981C] transition duration-200 ease-in-out hover:scale-105 hover:bg-[#D87E08]">{opt}</button>
             ))}
           </div>
 
-          <button onClick={() => {setActiveQuestion(activeQuestion => activeQuestion + 1), setSelectedAnswer(null)}} className="p-2.5 bg-white text-amber-500" >Next</button>
+          <button onClick={() => {setActiveQuestion(activeQuestion => activeQuestion + 1), setSelectedAnswer(null)}} className="cursor-pointer p-2.5 rounded-sm bg-[#1C7BF7] hover:bg-[#0862D8] mt-3.5 w-full" >Next</button>
         </div>
     </div>
     );
